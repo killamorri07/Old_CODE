ebb562c8608dce3d61e10211fc1f3fd29e2f5fbd
Lusia <34813844+killamorri07@users.noreply.github.com>
2023-12-07 23:36:54 +0800

    Create_find_change.c

--- Create_find_change.c
+++ Create_find_change.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-//Finding and change the word from original input setences.
+
 #define MAX_LENGTH 1000
+
+char* caseSensitiveStrStr(const char* haystack, const char* needle) {
+    while (*haystack) {
+        const char* h = haystack;
+        const char* n = needle;
+
+        while (*h && *n && (*h == *n)) {
+            h++;
+            n++;
+        }
+
+        if (!*n) {
+            return (char*)haystack;  // Needle found
+        }
+        haystack++;
+    }
+
+    return NULL;  // Needle not found
+}
 
 int replaceWords(char sentence[], char find[], char replace[], char newSentence[]) {
     char* position;
@@ -9,8 +28,8 @@ int replaceWords(char sentence[], char find[], char replace[], char newSentence[
     int lengthDifference = 0;
     int newSentenceIndex = 0;
 
-    // Find the first occurrence of the word to replace
-    while ((position = strstr(sentence, find)) != NULL) {
+    // Find the first occurrence of the word to replace (case-sensitive)
+    while ((position = caseSensitiveStrStr(sentence, find)) != NULL) {
         // Calculate the length difference before and after replacement
         lengthDifference += strlen(replace) - strlen(find);
 
@@ -27,9 +46,13 @@ int replaceWords(char sentence[], char find[], char replace[], char newSentence[
             // Move the sentence pointer to the next character after the replaced word
             sentence = position + strlen(find);
         } else {
-            // Copy the part of the original sentence before and including the replaced word
-            strncpy(newSentence + newSentenceIndex, sentence, position - sentence + strlen(find));
-            newSentenceIndex += position - sentence + strlen(find);
+            // Copy the part of the original sentence before the replaced word
+            strncpy(newSentence + newSentenceIndex, sentence, position - sentence);
+            newSentenceIndex += position - sentence;
+
+            // Copy the replacement word to the new sentence
+            strcpy(newSentence + newSentenceIndex, replace);
+            newSentenceIndex += strlen(replace);
 
             // Move the sentence pointer to the next character after the replaced word
             sentence = position + strlen(find);
@@ -47,20 +70,19 @@ int replaceWords(char sentence[], char find[], char replace[], char newSentence[
 
     return newSentenceIndex;
 }
-
-int main() {
+    int main() {
     char inputSentence[MAX_LENGTH];
     char inputFind[MAX_LENGTH];
     char inputReplace[MAX_LENGTH];
     char newSentence[MAX_LENGTH * 2]; // Increased buffer size to handle potential expansion
 
-    //printf("Enter an English sentence: ");
+    printf("Enter an English sentence: ");
     fgets(inputSentence, MAX_LENGTH, stdin);
 
-   // printf("Enter the word to find: ");
+    printf("Enter the word to find: ");
     fgets(inputFind, MAX_LENGTH, stdin);
 
-    //printf("Enter the replacement word: ");
+    printf("Enter the replacement word: ");
     fgets(inputReplace, MAX_LENGTH, stdin);
 
     // Remove newline characters
@@ -71,6 +93,7 @@ int main() {
     int newLength = replaceWords(inputSentence, inputFind, inputReplace, newSentence);
 
     printf("Modified Sentence:\n%s\n", newSentence);
+    printf("New Length of the Sentence: %d\n", newLength);
 
     return 0;
 }
